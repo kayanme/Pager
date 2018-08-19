f30c5a0d0bc62d8b8300bdfc09ee68db488ce7c7
kayanme <kayan@kayan.me>
2018-08-19 23:15:06 +0300

    CI fixes

--- package-build.sh
+++ package-build.sh
@@ -1,2 +1,2 @@
 dotnet build System.IO.Paging.PhysicalLevel.Core -c Release 
-dotnet nuget push System.IO.Paging.PhysicalLevel.Core/bin/Release/*.nupkg 
+dotnet nuget push System.IO.Paging.PhysicalLevel.Core/bin/Release/*.nupkg -k oy2cqotpghuxh56wruoabzcnpnh7z3auxcbbc5y26tngtu
