b62446de312cd58473bbdf1f014295d207e1cc42
Игорь Моисеев <kayan@kayan.me>
2019-02-13 23:28:14 +0300

    fixed configration docs

--- System.IO.Paging.PhysicalLevel/Configuration/Builder/IHeaderDefinition.cs
+++ System.IO.Paging.PhysicalLevel/Configuration/Builder/IHeaderDefinition.cs
@@ -1,8 +1,22 @@
 namespace System.IO.Paging.PhysicalLevel.Configuration.Builder
 {
+    /// <summary>
+    /// Header definition.
+    /// </summary>
+    /// <typeparam name="THeaderType">Header type</typeparam>
     public interface IHeaderDefinition<THeaderType> where THeaderType :  new()
     {
+        /// <summary>
+        /// Serialize header into target byte array.
+        /// </summary>
+        /// <param name="record">Header</param>
+        /// <param name="targetArray">Target array.</param>
         void FillBytes(ref THeaderType record, byte[] targetArray);
+        /// <summary>
+        /// Deserialize byte array into header
+        /// </summary>
+        /// <param name="sourceArray"></param>
+        /// <param name="record"></param>
         void FillFromBytes(byte[] sourceArray,ref THeaderType record);
         int Size { get; }
     }
--- System.IO.Paging.PhysicalLevel/Diagnostics/Tracing.cs
+++ System.IO.Paging.PhysicalLevel/Diagnostics/Tracing.cs
@@ -8,12 +8,7 @@ namespace System.IO.Paging.Diagnostics
     internal static class Tracing
     {
         internal static readonly DiagnosticSource Tracer = new DiagnosticListener("System.IO.Paging.Physical");
-        public static void Trace(string name,object value)
-        {
-            if (Tracer.IsEnabled(name))
-                Tracer.Write(name, value);
-
-        }      
+     
     }
 }
 
